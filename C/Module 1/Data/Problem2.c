77a2a4494e2375f7cc8fa49c2ff0b47fae44e67d
gt-ayush <gt-ayush@outlook.com>
2026-01-22 00:36:02 +0530

    Module 1.3: User Input (scanf) Done

---
+++ C/Module 1/Data/Problem2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int num1,num2,sum=0;
+    printf("Enter the first no.: ");
+    scanf("%d", &num1);
+    printf("Enter the Second no.: ");
+    scanf("%d", &num2);
+    sum=num1+num2;
+    printf("The sum of %d and %d is %d",num1,num2,sum);
+
+
+}
