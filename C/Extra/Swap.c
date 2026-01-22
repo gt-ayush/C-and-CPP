417aed9d75ebe87b86b952e1d441ce203c964d6e
gt-ayush <gt-ayush@outlook.com>
2026-01-22 11:56:56 +0530

    class 5

--- C/Extra/Swap.c
+++ C/Extra/Swap.c
@@ -2,10 +2,12 @@
 
 int main () {
 
-    int a = 0 , b=90;
+    int a = 8 , b=90;
     printf("%d , %d\n",a,b);
-    a=a+b;
-    b=a-b;
-    a=a-b ;
+    a=a^b;
+    printf("%d , %d\n",a,b);
+    b=a^b;
+    printf("%d , %d\n",a,b);
+    a=a^b ;
     printf("%d , %d",a,b);
 }
