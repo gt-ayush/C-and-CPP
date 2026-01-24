5aa4892393864a40c925a9d5a865da47572a792c
gt-ayush <gt-ayush@outlook.com>
2026-01-24 05:49:14 +0530

    Module 4.2 and PC1 fix

--- C/Module 4/data/PC1.c
+++ C/Module 4/data/PC1.c
@@ -2,12 +2,12 @@
 
 int main() {
 
-    int n[4];
-    for (int i = 0 ; i <= 4 ; i++){
+    int n[5];
+    for (int i = 0 ; i < 5 ; i++){
         printf("Enter the %d no. : ", i);
         scanf("%d" , &n[i]);
     }
-    for (int j = 0 ; j<=4 ; j++ ) {
+    for (int j = 0 ; j< 5 ; j++ ) {
         printf("%d\n",n[j]);
     }
 }
