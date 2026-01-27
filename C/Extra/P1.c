5be0247202371c2c3d3ca361a134acd432b7e5b6
gt-ayush <gt-ayush@outlook.com>
2026-01-27 11:42:54 +0530

    Extra Practic 1

--- C/Extra/P1.c
+++ C/Extra/P1.c
@@ -7,5 +7,12 @@ int main () {
     printf("%d , %d\n",a,b);
 
     b = a++;
-    printf("%d , %d",a,b);
+    printf("%d , %d\n",a,b);
+
+    printf("%d %d ",++a,a++);
+
+    int c = ++b + b++;
+    printf("%d",c);
+
+    
 }
---
+++ C/Extra/p2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main () {
+
+    int a = 1 ,b = 1 ,c;
+    c= a++ + b;
+    printf("%d, %d, %d",a,b,c);
+} 
+
+// < 
+// >
+// <=
+// >=
+// ==
+// !=
