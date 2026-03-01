13b0c5c0e0dd15a3c71e9060b2c8fdd960f76c93
gt-ayush <gt-ayush@outlook.com>
2026-03-02 05:26:06 +0530

    Class pr

---
+++ C/Extra/fib.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int fib ( int i ) {
+    if (i<0) return i;
+    if (i==0) return 0;
+    if (i==1) return 1;
+    return fib(i-1) + fib ( i-2 );
+
+}
+
+void main() {printf("%d",fib (3));}
---
+++ C/Module 10/data/main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "math_utils.h"
+void main (){
+
+}
