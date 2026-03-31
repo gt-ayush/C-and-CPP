6c291b683084f56f07dc64232e0e61195b319009
gt-ayush <gt-ayush@outlook.com>
2026-03-31 11:26:22 +0530

    bubble

---
+++ C/Extra/bubble short.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main () {
+    int x[10] =  {1,3,24,53,24,54,76,23,90,100};
+    for (int i = 10;i>=1;i--){
+        for ( int j = 0 ; j<=i ; j ++) {
+            if ( x[j] > x[j+1]) {
+                x[j]   ^= x[j+1];
+                x[j+1] ^= x[j];
+                x[j]   ^= x[j+1]; 
+            }
+        }
+    }
+    for (int i = 0 ;i<=10 ;i++) {
+        printf("%d",x[i]);
+    }
+}
