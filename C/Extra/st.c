f2a4cc7e33e949cd4b959ae40fc470b78e5ec678
gt-ayush <gt-ayush@outlook.com>
2026-04-29 04:07:47 +0530

    struct #3

--- C/Extra/st.c
+++ C/Extra/st.c
@@ -3,14 +3,20 @@ struct st
 {
     char name[50];
     char dig[50];
-    char date;
     int emp_id;
     float sal;
 };
 
 
 void main () {
-    struct st el;
+    struct st el={"Jhon Lest","Renega",1298,169890.00};
     
-printf("%d",sizeof(el));
+printf("%d\n",sizeof(el));
+printf("%s %s %d %.2f\n",el.name,el.dig,el.emp_id,el.sal);
+
+struct st e2;
+fgets(e2.name,sizeof(e2.name),stdin);
+
+printf("%s",e2.name);
+
 }
