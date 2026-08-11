4e252dbbeaf1b15cb37ab1a994407e8107dcee3f
gt-ayush <gt-ayush@outlook.com>
2026-08-11 12:32:22 +0530

    Added .gitignore , improved the greatest function

--- CPP/Extra/IO.cpp
+++ CPP/Extra/IO.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 using namespace std;
-
+void greatest(int a, int b, int c){
+    if(a>b && a>c)
+        cout<<a;
+    else if(b>a && b>c)
+        cout<<b;
+    else
+        cout<<c;
+}
 int main(){
     int a,b,c;
     cin>>a>>b>>c;
-    if(a>b && a>c)
-    {cout<<a;}
-    else if (b>a && b>c)
-    {cout<<b;}
-    else
-    {cout<<c;}
+    greatest(a,b,c);
 }
