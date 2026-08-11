181817b3d9a69d3a18a68c248ebc38d2f9cf478a
gt-ayush <gt-ayush@outlook.com>
2026-08-11 13:26:58 +0530

    Fibonacci

---
+++ CPP/Extra/fibo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;    
+
+int main(){
+    int n;
+    cin>>n;
+    int t1=0, t2=1, nt=0;
+    cout<<"Fibonacci Series: ";
+    for (int i = 1; i <= n; ++i){
+        if(i == 1){
+            cout<<t1<<" ";
+            continue;
+        }
+        if(i == 2){
+            cout<<t2<<" ";
+            continue;
+        }
+        nt = t1 + t2;
+        t1 = t2;
+        t2 = nt;
+        cout<<nt<<" ";
+    }
+    return 0;
+}
