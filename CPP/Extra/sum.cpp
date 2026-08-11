9fa440ad5ab62a64541f052ac154393a72641718
gt-ayush <gt-ayush@outlook.com>
2026-08-11 13:03:42 +0530

    Finding sum of n natural no.

---
+++ CPP/Extra/sum.cpp
@@ -0,0 +1,16 @@
+//find the sum of n naturnal no.
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, sum = 0;
+    cout << "Enter a natural number: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; ++i) {
+        sum += i;
+    }
+
+    cout << "The sum of the first " << n << " natural numbers is: " << sum << endl;
+    return 0;
+}
