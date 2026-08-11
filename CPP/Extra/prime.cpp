cd7e21415d83282d98deece6518f514b2aacf407
gt-ayush <gt-ayush@outlook.com>
2026-08-11 13:12:33 +0530

    Finding prime no.

---
+++ CPP/Extra/prime.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    int a; cin>>a;
+    for (int i=2; i<(a/2); i++){
+        if(a%i == 0){
+            cout << "Not Prime no. "<< endl;
+            return 0;
+        }
+    }
+    cout << "Prime no. "<< endl;
+    return 0;
+}
