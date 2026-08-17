932cb80fbd9a2fb8a25b8494e41ca332bf111594
gt-ayush <gt-ayush@outlook.com>
2026-08-17 06:34:50 +0530

    add element as start

---
+++ CPP/Extra/sttarr.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //---
+    int arr[10];
+    for(int i=0;i<5;i++){
+        cout<<"Enter the Element : ";
+        cin>>arr[i];
+    }
+    //---
+    cout<<"ENter Element to be inserted : ";
+    int a;
+    cin>>a;
+    //---
+    for (int i=5;i>0;i--) {arr[i]=arr[i-1];}
+    arr[0]=a;
+    //---
+    for (int i=0;i<6;i++){
+        cout<<arr[i]<<endl;
+    }
+}
