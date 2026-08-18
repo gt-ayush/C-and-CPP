29abd576232f0286fb0f7353fabf3b30018c7cf7
gt-ayush <gt-ayush@outlook.com>
2026-08-18 14:33:20 +0530

    deleting an element at the end

---
+++ CPP/Extra/endelarr.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //---
+    int arr[10];
+    for(int i=0;i<5;i++){
+        cout<<"Enter the Element : ";
+        cin>>arr[i];
+    }
+    //---
+    for (int i=0;i<4;i++){
+        cout<<arr[i]<<endl;
+    }
+    //--
+}
