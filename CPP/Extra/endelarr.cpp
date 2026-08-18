f01ee7c2d2f496c5734ecb7305a2fa66c8c35a19
gt-ayush <gt-ayush@outlook.com>
2026-08-18 15:21:08 +0530

    edge case fix

--- CPP/Extra/endelarr.cpp
+++ CPP/Extra/endelarr.cpp
@@ -11,8 +11,12 @@ int main(){
     int index;
     cin>>index;
     //---
+    if(index<10){
     for (int i=index;i<5;i++){
         arr[i-1]=arr[i];
+    }}
+    else{
+        cout<<"Index is out of range"<<endl;
     }
     //---
     for (int i=0;i<4;i++){
