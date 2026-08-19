9df4cc3cac4612200022d4d819f3c9e1efa725b5
gt-ayush <gt-ayush@outlook.com>
2026-08-19 14:54:45 +0530

    taken n no of inputes

--- CPP/Extra/linbin.cpp
+++ CPP/Extra/linbin.cpp
@@ -29,7 +29,7 @@ int main() {
     int n;
     cout << "Enter the number of elements: ";
     cin >> n;
-    int arr[100];
+    int arr[n];
     cout << "Enter the elements in sorted order: ";
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
