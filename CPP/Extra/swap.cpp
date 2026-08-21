d6a290ed820560db7a72f1a82abd1ed8937c9550
gt-ayush <gt-ayush@outlook.com>
2026-08-21 11:43:24 +0530

    pass by reference

---
+++ CPP/Extra/swap.cpp
@@ -0,0 +1,20 @@
+//swap using function 
+
+#include <iostream>
+using namespace std;
+
+void swap(int &a, int &b) //pass by reference
+{
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+int main()
+{
+    int x = 10, y = 20;
+    cout << "Before swapping: x = " << x << ", y = " << y << endl;
+    swap(x, y);
+    cout << "After swapping: x = " << x << ", y = " << y << endl;
+    return 0;
+}
