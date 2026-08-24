82f130f4339f52ee296dfbfb4e590603284ae7bd
gt-ayush <gt-ayush@outlook.com>
2026-08-24 14:55:40 +0530

    sum of n natural no

---
+++ CPP/Extra/sonnno.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int sum(int n){
+    if(n<=1)
+        return n;
+    return n+sum(n-1);
+}
+int main() {
+    int n;
+    cout<<"Enter the number of terms : ";
+    cin>>n;
+    cout<<"Sum: "<<sum(n)<<endl;
+}
