4950a5809b1ed0d6f632f3dffed24a8cfa9b15ad
gt-ayush <gt-ayush@outlook.com>
2026-08-25 13:04:34 +0530

    testing friends keyword

---
+++ CPP/Extra/frien.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+//testing friend keyword
+
+class fri(){
+
+    int a;
+    public:
+        fri(int x){
+            a = x;
+        }
+        friend void display(fri f);
+};
+void display(fri f){
+    cout<<"The value of a is: "<<f.a<<endl;
+}
+int main(){
+    fri f1(10);
+    display(f1);
+    return 0;
+}
