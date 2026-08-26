b9258959aad82663197f2b96916570f89235b90e
gt-ayush <gt-ayush@outlook.com>
2026-08-26 15:50:18 +0530

    done

--- CPP/Extra/bs.cpp
+++ CPP/Extra/bs.cpp
@@ -13,11 +13,9 @@ int main() {
         cin >> arr[i];
     }
 
-    // Bubble sort algorithm
     for (int i = 0; i < n-1 ; i++) {
         for (int j = 0; j < n - i - 1; j++) {
             if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
                 arr[j] ^= arr[j + 1];
                 arr[j + 1] ^= arr[j];
                 arr[j] ^= arr[j + 1];
