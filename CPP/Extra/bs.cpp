3e309a9516c87e51a35b497fd61d08d885b024a5
gt-ayush <gt-ayush@outlook.com>
2026-08-26 15:39:38 +0530

    bubble sort

---
+++ CPP/Extra/bs.cpp
@@ -0,0 +1,35 @@
+//bubble sort
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the length of the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Bubble sort algorithm
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << ' ';
+    }
+    cout << '\n';
+
+    return 0;
+}
