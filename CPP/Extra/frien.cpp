3de8b07d55eb1c26ca1119f4a86e2362b31b266f
gt-ayush <gt-ayush@outlook.com>
2026-08-28 11:09:16 +0530

    friends with 3 funcrion

--- CPP/Extra/frien.cpp
+++ CPP/Extra/frien.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 //testing friend keyword
 
-//use 2 friend functions to access private members of a class
+//use 3 friend functions to access private members of a class
 
 class fri {
     friend void display(fri f);
     friend void update(fri &f, int x);
+    friend void print(const fri &f);
     int a;
     public:
         fri(int x){
@@ -23,10 +24,15 @@ void update(fri &f, int x){
     f.a = x;
 }
 
+void print(const fri &f){
+    cout<<"The value of a is: "<<f.a<<endl;
+}
+
 int main(){
     fri f1(10);
     display(f1);
     update(f1, 20);
     display(f1);
+    print(f1);
     return 0;
 }
