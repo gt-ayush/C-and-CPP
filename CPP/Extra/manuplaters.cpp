5c5f72ffab717580d4519849078c9be9cbbb6a6a
gt-ayush <gt-ayush@outlook.com>
2026-08-31 15:23:07 +0530

    munuplaters1

---
+++ CPP/Extra/manuplaters.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main () {
+    cout << "Hello, World!" << endl;
+    int a=10;
+    cout<<"The value of a is: "<<a<<endl;
+    float b=10.538481;
+    cout<<"The value of b is: "<<setprecision(2)<<b<<endl;
+    return 0;
+}
