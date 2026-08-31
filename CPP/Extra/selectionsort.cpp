145cd3b4756aafccfaab8c5660708a8747dca007
gt-ayush <gt-ayush@outlook.com>
2026-08-31 12:20:54 +0530

    selection sort

---
+++ CPP/Extra/selectionsort.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void selectionsort(vector<int> &a,int n){
+    for (int i=0;i<n;i++){
+        int min=i;
+        for(int j=i+1;j<n;j++){
+            if(a[j]<a[min]){
+                min=j;
+            }
+        }
+        swap(a[i],a[min]);
+    }
+}   
+
+int main () {
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    selectionsort(a,n);
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<" ";
+    }
+    return 0;
+}
