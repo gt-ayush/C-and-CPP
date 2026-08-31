35065613fef4f964d9fa6f41161c04a440832915
gt-ayush <gt-ayush@outlook.com>
2026-08-31 15:30:12 +0530

    setw

--- CPP/Extra/manuplaters.cpp
+++ CPP/Extra/manuplaters.cpp
@@ -6,6 +6,7 @@ int main () {
     cout << "Hello, World!" << endl;
     int a=10;
     cout<<"The value of a is: "<<a<<endl;
+    cout<<"The value of a is: "<<setw(4)<<a<<endl;
     float b=10.538481;
     cout<<"The value of b is: "<<setprecision(2)<<b<<endl;
     cout<<"The value of b is: "<<fixed<<setprecision(2)<<b<<endl;
