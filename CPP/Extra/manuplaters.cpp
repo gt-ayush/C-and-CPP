7f1c6267cdaceae81c8a7452038f908b66619d3f
gt-ayush <gt-ayush@outlook.com>
2026-08-31 15:46:18 +0530

    setbase

--- CPP/Extra/manuplaters.cpp
+++ CPP/Extra/manuplaters.cpp
@@ -11,11 +11,12 @@ int main () {
     cout<<"The value of a is: "<<oct<<a<<endl;
     cout<<"The value of a is: "<<hex<<a<<endl;
     cout<<"The value of a is: "<<dec<<a<<endl;
+    cout<<"The value of a is: "<<setbase(8)<<a<<endl;
     float b=10.538481;
     cout<<"The value of b is: "<<setprecision(2)<<b<<endl;
     cout<<"The value of b is: "<<fixed<<setprecision(2)<<b<<endl;
     double c=8.09432232345;
     char g='a';
-    
+
     return 0;
 }
