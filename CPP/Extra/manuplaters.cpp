f406c25ed7054eac45d8c47f27a0f9e4cfba46c8
gt-ayush <gt-ayush@outlook.com>
2026-08-31 15:32:11 +0530

    dec

--- CPP/Extra/manuplaters.cpp
+++ CPP/Extra/manuplaters.cpp
@@ -10,6 +10,7 @@ int main () {
     cout<<"The value of a is: "<<setw(4)<<setfill('0')<<a<<endl;
     cout<<"The value of a is: "<<oct<<a<<endl;
     cout<<"The value of a is: "<<hex<<a<<endl;
+    cout<<"The value of a is: "<<dec<<a<<endl;
     float b=10.538481;
     cout<<"The value of b is: "<<setprecision(2)<<b<<endl;
     cout<<"The value of b is: "<<fixed<<setprecision(2)<<b<<endl;
