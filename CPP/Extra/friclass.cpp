923a50d13d1bd5193e38f8bd4316bf7e11dcf990
gt-ayush <gt-ayush@outlook.com>
2026-08-31 15:00:35 +0530

    friend of class is a class

--- CPP/Extra/friclass.cpp
+++ CPP/Extra/friclass.cpp
@@ -12,7 +12,7 @@ class A{
 };
 
 class B{
-    int x,y;
+    int x=9,y=90;
     public:
     void setdata(int m,int n,A &o){
         o.a=m;
@@ -22,7 +22,11 @@ class B{
         cout<<"a="<<o.a<<endl;
         cout<<"b="<<o.b<<endl;
     }
-
+    //function without the class object as a parameter
+    void area(){
+        cout<<"Area of rectangle is: "<<x*y<<endl;
+    }
+    
 };
 
 int main()
@@ -31,5 +35,8 @@ int main()
     B b;
     a.getdata(10, 20);
     b.showdata(a);
+    b.area();
     return 0;
 }
+
+// &o is used to pass the object of class A by reference to the member functions of class B. This allows the functions in class B to access and modify the private members of class A directly, since class B is declared as a friend of class A.
