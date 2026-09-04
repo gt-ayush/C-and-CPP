31a5973524e5353293164ee44a4652053b36917b
gt-ayush <gt-ayush@outlook.com>
2026-09-04 06:00:09 +0530

    even elemants

--- CPP/Extra/unit2/arr.cpp
+++ CPP/Extra/unit2/arr.cpp
@@ -24,5 +24,13 @@ int main() {
     cout << "Size of array is: " << sizeof(arr1) / sizeof(arr1[0]) << endl;
     cout << "Size of array is: " << sizeof(arr)  << endl;
     cout << "Size of array is: " << sizeof(arr1) << endl;
+
+    //even elements
+    cout << "Even elements are: ";
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] % 2 == 0) {
+            cout << arr[i] << " ";
+        }
+    }
     return 0;
 }
