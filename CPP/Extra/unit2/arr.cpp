df414e314853084c03e578df62445a66f5872813
gt-ayush <gt-ayush@outlook.com>
2026-09-04 05:53:15 +0530

    reverse order

--- CPP/Extra/unit2/arr.cpp
+++ CPP/Extra/unit2/arr.cpp
@@ -5,14 +5,14 @@ int main() {
     int arr[5] = {1, 2, 3, 4, 5};
     int arr1[5];
     cout << "Array elements are: ";
-    for (int i = 0; i < 5; i++) {
+    for (int i = 4; i >= 0; i--) {
         cout << arr[i] << " ";
     }
     cout << endl;
     for (int i = 0; i < 5; i++) {
         cin >> arr1[i];
     }
-    for (int i = 0; i < 5; i++) {
+    for (int i = 4; i >= 0; i--) {
         cout << arr1[i] << " ";
     }
     cout << endl;
