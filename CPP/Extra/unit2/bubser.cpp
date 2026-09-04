1451853a33e1978e2c20e0593ccf80fa819a8577
gt-ayush <gt-ayush@outlook.com>
2026-09-04 06:35:33 +0530

    assending order

--- CPP/Extra/unit2/bubser.cpp
+++ CPP/Extra/unit2/bubser.cpp
@@ -7,7 +7,7 @@ int main() {
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
-    // Bubble Sort
+    // Bubble Sort in asscending order
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
             if (arr[j] > arr[j+1]) {
