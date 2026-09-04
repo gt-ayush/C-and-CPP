d6051597dc073d42b3744001c84527734d0d7254
gt-ayush <gt-ayush@outlook.com>
2026-09-04 05:51:09 +0530

    two arr

--- CPP/Extra/unit2/arr.cpp
+++ CPP/Extra/unit2/arr.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 int main() {
     int arr[5] = {1, 2, 3, 4, 5};
-
+    int arr1[5];
     cout << "Array elements are: ";
     for (int i = 0; i < 5; i++) {
         cout << arr[i] << " ";
     }
     cout << endl;
-
-    arr[5]=7; // This line will cause undefined behavior since it is accessing out of bounds of the array.
-
+    for (int i = 0; i < 5; i++) {
+        cin >> arr1[i];
+    }
+    for (int i = 0; i < 5; i++) {
+        cout << arr1[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
