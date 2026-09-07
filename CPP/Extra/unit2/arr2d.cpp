788e50b5538d3475dd74ba8221e2f141f5608f5d
gt-ayush <gt-ayush@outlook.com>
2026-09-07 10:10:28 +0530

    transpose of a 2D array

--- CPP/Extra/unit2/arr2d.cpp
+++ CPP/Extra/unit2/arr2d.cpp
@@ -20,5 +20,13 @@ int main () {
         }
         cout<<endl;
     }
+
+    // transpose of a 2D array
+    for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            cout<<arr[j][i]<<" ";
+        }
+        cout<<endl;
+    }
     return 0;
 }
