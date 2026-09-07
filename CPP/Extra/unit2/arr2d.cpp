3a4a51792852356c68a9cbd3c02d2f999601e3ad
gt-ayush <gt-ayush@outlook.com>
2026-09-07 10:06:36 +0530

    dignol ele

--- CPP/Extra/unit2/arr2d.cpp
+++ CPP/Extra/unit2/arr2d.cpp
@@ -3,10 +3,22 @@ using namespace std;
 
 int main () {
     int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    int sum=0;
     for(int i=0;i<3;i++){
         for(int j=0;j<3;j++){
             cout<<arr[i][j]<<" ";
+            sum+=arr[i][j];
         }
         cout<<endl;
     }
+    cout<<"Sum: "<<sum<<endl;
+        for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            if(i==j){
+                cout<<arr[i][j]<<" ";
+            }
+        }
+        cout<<endl;
+    }
+    return 0;
 }
