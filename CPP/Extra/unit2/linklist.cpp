8e49600478077591367e75620338c48b2b537f47
gt-ayush <gt-ayush@outlook.com>
2026-09-07 06:46:30 +0530

    uni directional linklist in structure without using new keyword

--- CPP/Extra/unit2/linklist.cpp
+++ CPP/Extra/unit2/linklist.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//uni directional linklist in structure without using new keyword
 struct List{
     int data;
     List *next;
@@ -12,21 +12,16 @@ struct List{
 
 int main(){
     
-    List *head, *n2, *n3, *n4;
-    
-    head = new List(10);
-    n2 = new List(20);
-    n3 = new List(30);
-    n4 = new List(40);
+    List head(10), n2(20), n3(30), n4(40);
 
-    head->next = n2;
-    n2->next = n3;
-    n3->next = n4;
-    n4->next = NULL;
+    head.next = &n2;
+    n2.next = &n3;
+    n3.next = &n4;
+    n4.next = NULL;
 
     cout << "The linked list is: ";
-    cout << head->data << " ";
-    cout << head->next->data << " ";
-    cout << head->next->next->data << " ";
-    cout << head->next->next->next->data << " ";
+    cout << head.data << " ";
+    cout << head.next->data << " ";
+    cout << head.next->next->data << " ";
+    cout << head.next->next->next->data << " ";
 }
