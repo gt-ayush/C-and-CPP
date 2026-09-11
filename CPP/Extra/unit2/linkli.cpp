dfe72ee82ff00fbf7200f7caa7383a50ad86e55c
gt-ayush <gt-ayush@outlook.com>
2026-09-11 05:45:03 +0530

    i don't know

---
+++ CPP/Extra/unit2/linkli.cpp
@@ -0,0 +1,23 @@
+// Linklist Implementation
+// insertion at beginning, end and at a given position
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node(int value){
+        data=value;
+        next=nullptr;
+    }
+};
+
+class work{
+    
+};
+
+int main(){
+    Node *head = nullptr;
+    return 0;
+}
