4c600f334182a770a73c2334b46164cff942ceb8
gt-ayush <gt-ayush@outlook.com>
2026-09-21 10:05:50 +0530

    explain error

--- CPP/Extra/unit3/f.cpp
+++ CPP/Extra/unit3/f.cpp
@@ -6,14 +6,15 @@ int main  ()
     ofstream fout;
     ifstream fin;
     fout.open("sample.txt");
-    fout<<"Hello World";
+    fout<<"Hello World\0";
     fout.close();
     fin.open("sample.txt");
     char c;
     while(fin)
     {
         fin.get(c);
-        cout<<c;
+        cout<<c; 
+        //it will print dublicat element at end because after reading last element it will check for eof and it will be false so it will print last element again
     }
     fin.close();
 }
