3a71bd976ffa43d1e0db60a5a0dde8d949ca2293
gt-ayush <gt-ayush@outlook.com>
2026-09-23 09:12:36 +0530

    Queue

---
+++ CPP/Extra/unit3/queue.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node *next;
+};
+
+class Queue{
+    Node *front;
+    Node *rear;
+    public:
+        Queue(){
+            front = nullptr;
+            rear = nullptr;
+        }
+        void enqueue(int value){  // Add an element to the end of the queue
+            Node *newNode = new Node();
+            newNode->data = value;
+            newNode->next = nullptr;
+
+            if(rear == nullptr){
+                front = newNode;
+                rear = newNode;
+            } else {
+                rear->next = newNode;
+                rear = newNode;
+            }
+        }
+
+        void dequeue(){  // Remove an element from the front of the queue
+            if(front == nullptr){
+                cout << "Queue is empty" << endl;
+                return;
+            }
+            Node *temp = front;
+            front = front->next;
+            delete temp;
+
+            if(front == nullptr){
+                rear = nullptr;
+            }
+        }
+
+        void display(){
+            Node *temp = front;
+            while(temp != nullptr){
+                cout << temp->data << " ";
+                temp = temp->next;
+            }
+            cout << endl;
+        }
+};
+
+int main(){
+    Queue q;
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.display();
+    q.dequeue();
+    q.display();
+    return 0;
+}
