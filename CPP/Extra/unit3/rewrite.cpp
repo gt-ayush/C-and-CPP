be32abf5b20d94a0e20b9f3c7dcafa89f1ecc968
gt-ayush <gt-ayush@outlook.com>
2026-09-25 12:03:38 +0530

    copy from one to another

---
+++ CPP/Extra/unit3/rewrite.cpp
@@ -0,0 +1,51 @@
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+class table{
+    int no;
+    public:
+    void getdata(){
+        cout<<"Enter no."<<endl;
+        cin>>no;
+    }
+    void savetable(){
+        ofstream fout;
+        fout.open("C:\\Users\\Ayush Kumar Gupta\\Desktop\\Repo\\C-and-CPP\\CPP\\Extra\\unit3\\table.txt");
+            for(int i=1;i<=10;i++){
+                fout<<no<<" * "<<i<<" = "<<no*i<<endl;  
+        }
+        fout<<endl;
+        fout.close();
+    }
+    void displaytable(){
+        ifstream fin;
+        fin.open("C:\\Users\\Ayush Kumar Gupta\\Desktop\\Repo\\C-and-CPP\\CPP\\Extra\\unit3\\table.txt");
+        ifstream fin1;
+        ofstream fout;
+        fout.open("C:\\Users\\Ayush Kumar Gupta\\Desktop\\Repo\\C-and-CPP\\CPP\\Extra\\unit3\\table1.txt");
+        if(!fin && !fin1){
+            cout<<"File not found"<<endl;
+            return;
+        }
+        string line;
+        while(getline(fin,line)){
+            fout<<line<<endl;
+        }
+        fin.close();
+        fout.close();
+    }
+};
+
+
+
+
+int main  ()  
+{
+    table s; 
+    
+        s.getdata();
+        s.savetable();
+        s.displaytable();
+}
